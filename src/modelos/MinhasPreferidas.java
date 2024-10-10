c9d7c546e3a0191c9989b791a42f2bc50b07d84a
gustavocravo <gustavocravo.profissional@gmail.com>
2024-10-10 11:24:13 -0300

    Realizacao de exercicio

---
+++ src/modelos/MinhasPreferidas.java
@@ -0,0 +1,12 @@
+package modelos;
+
+public class MinhasPreferidas {
+
+    public void inclui(Audio audio) {
+        if (audio.getClassificacao() >= 8) {
+            System.out.println(audio.getTitulo() + " é considerado sucesso absoluto e preferido por todos!");
+        } else {
+            System.out.println(audio.getTitulo() + " tambem é um dos que todo mundo está curtindo");
+        }
+    } 
+}
